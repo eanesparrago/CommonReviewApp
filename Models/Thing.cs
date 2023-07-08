2835fbcba0705f225908fc2e0caf3af22524b92f
eanesparrago <eanesparrago@gmail.com>
2023-07-08 22:17:25 +0800

    Add models

---
+++ Models/Thing.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonReviewApp.Models;
+
+public class Thing
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
+
